4542801efe0fa8d8f6baa0d081f2716bee1b6847
Anh Vu <honganhvu278@gmail.com>
2026-04-07 12:15:25 -0400

    fetch data from recurse api

--- index.ts
+++ index.ts
@@ -1,1 +1,30 @@
-console.log("Hello via Bun!");
+import { parseArgs } from "util";
+import { getRecurseData } from "./utils/rc-api.ts"
+
+
+const { values, positionals } = parseArgs({
+  args: Bun.argv,
+  strict: true,
+  allowPositionals: true,
+});
+
+
+const searchTerm = positionals[2]
+
+const RC_API_KEY = process.env.RC_API_KEY
+
+if (!RC_API_KEY) {
+    console.log("You need RC API key")
+    process.exit()
+} 
+
+if (!searchTerm) {
+    process.exit()
+
+}
+
+const data = await getRecurseData(searchTerm, RC_API_KEY)
+console.log(data);
+
+
+
---
+++ utils/rc-api.ts
@@ -0,0 +1,24 @@
+export async function getRecurseData(searchTerm: string, RC_API_KEY: string) {
+    const RecurseAPIURL = new URL("https://www.recurse.com/api/v1/profiles")
+    RecurseAPIURL.searchParams.set("scope", "current")
+    RecurseAPIURL.searchParams.set("limit", "2")
+    RecurseAPIURL.searchParams.set("query", searchTerm )
+    console.log(RecurseAPIURL.href)
+
+    // fetch RC API
+    const response = await fetch(RecurseAPIURL.href, {
+        method: "GET",
+        headers: {
+        "Authorization": `Bearer ${RC_API_KEY}`,
+        "Content-Type": "application/json",
+        },
+    });
+
+    const data = await response.json();
+    return data
+
+}
+
+
+
+
